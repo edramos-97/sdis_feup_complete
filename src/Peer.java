1b7dfa5bc638430eae00f808060ebc23d0994e66
Xavier Fontes <xfontes42@gmail.com>
2018-03-09 12:54:58 +0000

    update

--- src/Peer.java
+++ src/Peer.java
@@ -1,2 +1,11 @@
+
 public class Peer {
+
+    public static void main(String[] args) {
+//        regular peer:
+//          java Peer 0 <MC_addr> <MC_port> <MDB_addr> <MDB_port> <MDR_addr> <MDR_port>
+//        initiator peer:
+//          java Peer 1 <MC_addr> <MC_port> <MDB_addr> <MDB_port> <MDR_addr> <MDR_port> <sub_protocol> <opnd_1> <opnd_2>
+
+    }
 }
