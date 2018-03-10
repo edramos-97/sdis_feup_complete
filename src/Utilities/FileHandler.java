53bf09c2788b383cb0eb9afc455145c710b7b5d6
edramos-97 <up201505779@fe.up.pt>
2018-03-10 18:00:01 +0000

    save file chunk almost done

---
+++ src/Utilities/FileHandler.java
@@ -0,0 +1,71 @@
+package Utilities;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class FileHandler {
+
+    public static void main(String[] args){
+        try {
+            saveChunk("123","isto foi um ficheiro que eu criei onde eu quis",new char[]{'1'});
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private static String savePath = System.getProperty("user.home")+File.separator+"Desktop"+File.separator+"testFolder"+File.separator;
+
+
+    /**
+     *
+     * @param fileId - File identifier
+     * @param data - File chunk data
+     * @param chunkNo - File chunk sequence number
+     * @return True if the chunk was saved successfully, false otherwise.
+     * @throws IOException - Default IOException
+     */
+    public static boolean saveChunk(String fileId, String data, char[] chunkNo) throws IOException {
+        FileOutputStream out = null;
+
+        try {
+            String path = savePath+ new String(chunkNo)+".txt";
+            System.out.println(path);
+            File chunk = new File(path);
+            if (!chunk.getParentFile().mkdirs()){
+                throw new IOException("Could not make path for chunk no:"+new String(chunkNo)+" of file:"+fileId);
+            }
+            out = new FileOutputStream(chunk,false);
+            for (char c : data.toCharArray()) {
+                out.write(c);
+            }
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
+        }finally{
+            if (out != null) {
+                out.close();
+            }
+        }
+        return true;
+    }
+
+    public boolean backupFile(String path){
+        return true;
+    }
+
+    public boolean hasFile(char[] fileId){
+
+        return true;
+    }
+
+    public boolean checkFileIntegrity(char[] fileId){
+        return true;
+    }
+
+    public ArrayList<byte[]> splitFile(){
+        ArrayList<byte[]> fileChunks = new ArrayList<>();
+        return fileChunks;
+    }
+}
