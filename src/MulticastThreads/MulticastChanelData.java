7a0c8f60d38a13b48a64bab94c07a25c5b3fb4aa
Xavier Fontes <xfontes42@gmail.com>
2018-03-15 16:52:21 +0000

    updated peer and multicast chanels

--- src/MulticastThreads/MulticastChanelData.java
+++ src/MulticastThreads/MulticastChanelData.java
@@ -1,4 +1,25 @@
 package MulticastThreads;
 
+import java.io.IOException;
+import java.net.InetAddress;
+
 public class MulticastChanelData extends MulticastChanel {
+
+    public MulticastChanelData(String mcc_address, String mcc_port, String mcb_address,
+                               String mcb_port, String mcr_address, String mcr_port) throws IOException {
+
+        // creating common variables
+        super(mcc_address,mcc_port,mcb_address,mcb_port,mcr_address,mcr_port);
+
+        //joining group
+        multicast_data_socket.joinGroup(InetAddress.getByName(multicast_data_address));
+
+    }
+
+    @Override
+    public void run() {
+
+        // listen on data
+
+    }
 }
