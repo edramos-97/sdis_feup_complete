55346088e1fa2f9af23178731791f73015b06ded
edramos-97 <up201505779@fe.up.pt>
2018-04-02 15:35:55 +0100

    trying to fix tcp issue

--- src/InitiatorCommunication/RestoreEnhancement.java
+++ src/InitiatorCommunication/RestoreEnhancement.java
@@ -4,11 +4,11 @@ import Utilities.FileHandler;
 import Utilities.ProtocolMessage;
 import Utilities.ProtocolMessageParser;
 
-import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Arrays;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class RestoreEnhancement implements Runnable {
@@ -27,24 +27,22 @@ public class RestoreEnhancement implements Runnable {
             try {
 
                 System.out.println("Running and reading from socket");
+                //ServerSocket tcp_ss = new ServerSocket()
                 Socket receiving_socket = tcp_ss.accept();
 
-                BufferedReader inFromOtherPeer = new BufferedReader(new InputStreamReader(receiving_socket.getInputStream()));
+                //BufferedReader inFromOtherPeer = new BufferedReader(new InputStreamReader(receiving_socket.getInputStream()));
 
-                char[] read_chars = new char[FileHandler.MAX_SIZE_MESSAGE];
-                int actually_read = inFromOtherPeer.read(read_chars, 0, FileHandler.MAX_SIZE_MESSAGE);
+                DataInputStream in = new DataInputStream(receiving_socket.getInputStream());
+
+                byte[] read_chars = new byte[FileHandler.MAX_SIZE_MESSAGE];
+                int actually_read = in.read(read_chars, 0, FileHandler.MAX_SIZE_MESSAGE);
                 //System.out.println("READ SOME BYTES YE");
                 //System.out.println(read_chars);
                 //System.out.println(actually_read);
                 //System.out.println("READ SOME BYTES YEND");
 
-                byte[] read_bytes = new byte[actually_read];
-
-                for(int i = 0; i < actually_read; i++) {
-                    read_bytes[i] = (byte) read_chars[i];
-                }
-
-
+                byte[] read_bytes = Arrays.copyOfRange(read_chars, 0, actually_read);
+                
                 ProtocolMessage message = ProtocolMessageParser.parseMessage(read_bytes,read_bytes.length);
 
                 // Not getting here so not really receiving stuff on tcp connection
@@ -55,6 +53,7 @@ public class RestoreEnhancement implements Runnable {
                 //VolatileDatabase.restoreMemory.put(message.getFileId(),new Integer[]{Integer.parseInt(message.getChunkNo()),message.getBody().length});
                 //FileHandler.saveChunk(message,"restore");
 
+                receiving_socket.close();
 
             } catch (IOException e) {
                 System.out.println("There was an error accepting socket...");
